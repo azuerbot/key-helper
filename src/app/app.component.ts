6fa0df710775088fe10447d1966eb83b5a126e24
azuerbot <azuer-bot@outlook.com>
2019-01-27 23:19:23 -0500

    Timer implemented.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { timer } from 'rxjs/internal/observable/timer';
+import { Observable, Subscribable, Subscriber } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'libtest';
+  /**
+   *
+   */
+  constructor() {
+    // this.countTime();
+  }
+
+  public countTime() {
+    const expectedTime = 10;
+    let executor = timer(expectedTime * 1000);
+    let subscriber = executor.subscribe(() => {
+      console.log('It is time!!');
+    });
+
+    const counter = timer(1000, 1000)
+    .subscribe((second) => {
+        console.log('Second: ', second);
+
+        if (second === 5) {
+          subscriber.unsubscribe();
+          executor = timer(expectedTime * 1000);
+          console.log('Creating executor again!');
+
+          subscriber = executor.subscribe(() => {
+            console.log('It is time!!');
+          });
+        } else if (second === expectedTime) {
+          console.log('Unsubscribing counter!!');
+          counter.unsubscribe();
+        }
+    });
+  }
 }
